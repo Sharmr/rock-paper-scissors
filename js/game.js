86e2ce80d76e238c52c76eb89930ffbdd64b993d
s98raghav@gmail.com <s98raghav@gmail.com>
2021-06-13 20:36:24 -0700

    winner calculator

--- js/game.js
+++ js/game.js
@@ -1,9 +1,9 @@
-function randIntFromInterval(min, max) { //inclusive
-    return Math.floor(Math.random()*(max - min + 1));
+function randIntFromInterval(max) { //inclusive
+    return Math.floor(Math.random()*(max)) + 1;
 }
 
 function computerPlay() {
-    return randIntFromInterval(1,3);
+    return randIntFromInterval(3);
 }
 
 function playerInput() {
@@ -24,6 +24,22 @@ function playerInput() {
                 (count <= 4) ? alert("Invalid Input. Try Again"):alert("Imagine not being able to spell?"); 
         }
     }
-    
-
+}
+//Keep in mind, throughout this program: 1 is rock, 2 is paper, 3 is scissors. No exceptions
+function andTheWinnerIs (player, computer) {
+    if (player ==  computer) {
+        return "DRAW";
+    }
+    else if (player == 1 && computer == 3) {
+        return "YOU WIN";
+    }
+    else if (player == 3 && computer == 1) {
+        return "YOU LOSE";
+    }
+    else if (player > computer) {
+        return "YOU WIN";
+    }
+    else {
+        return "YOU LOSE";
+    }
 }
